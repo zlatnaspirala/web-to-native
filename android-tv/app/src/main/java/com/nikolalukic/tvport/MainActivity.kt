5969549fa13449ff70b69e514d03b78794821bc7
zlatnaspirala <zlatnaspirala@gmail.com>
2026-06-24 13:22:22 +0200

    Update MainActivity.kt

--- android-tv/app/src/main/java/com/nikolalukic/tvport/MainActivity.kt
+++ android-tv/app/src/main/java/com/nikolalukic/tvport/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nikolalukic.tvport
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
@@ -11,11 +12,16 @@ import androidx.fragment.app.FragmentActivity
 
 class MainActivity : FragmentActivity() {
 
+
+
     private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        android.webkit.WebView.setDataDirectorySuffix("main")
+        WebView.setWebContentsDebuggingEnabled(true)
 
         webView = WebView(this)
         setContentView(webView)
@@ -27,12 +33,21 @@ class MainActivity : FragmentActivity() {
             }
         })
 
+
+
         val s: WebSettings = webView.settings
         s.javaScriptEnabled = true
         s.domStorageEnabled = true
         s.mediaPlaybackRequiresUserGesture = false
+        // add:
+        s.setRenderPriority(WebSettings.RenderPriority.HIGH)
+//      webView.loadUrl("https://maximumroulette.com/apps/webgpu/examples.html")
+        webView.loadUrl("chrome://flags")
 
-        webView.loadUrl("https://maximumroulette.com/apps/webgpu/examples.html")
+        webView.evaluateJavascript("navigator.gpu ? 'GPU EXISTS' : 'NO GPU'") { result ->
+            Log.d("WEBGPU", result)
+        }
+
     }
 
     override fun onBackPressed() {
